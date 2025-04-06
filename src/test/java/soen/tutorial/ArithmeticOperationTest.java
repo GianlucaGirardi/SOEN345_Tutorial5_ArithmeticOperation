36bc751b5b9232b260f6388a0cbbc94a2b04b6de
Luca <100626835+GianlucaGirardi@users.noreply.github.com>
2025-04-06 09:22:18 -0400

    Redo partc for dashboard screenshot

--- src/test/java/soen/tutorial/ArithmeticOperationTest.java
+++ src/test/java/soen/tutorial/ArithmeticOperationTest.java
@@ -3,7 +3,7 @@ package soen.tutorial;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+// import static org.junit.Assert.fail;
 
 public class ArithmeticOperationTest {
 
@@ -15,18 +15,18 @@ public class ArithmeticOperationTest {
 		ArithmeticOperation operations = new ArithmeticOperation();
 		Integer actual = operations.addOrSub(2, 6);
 		Integer expected = 8;
-		// assertEquals(expected, actual);
-		fail("Must fail");
+		assertEquals(expected, actual);
+		// fail("Must fail");
 	}
-    // @Test
-    // public void testSub()
-    // {
+    @Test
+    public void testSub()
+    {
 
-    //     ArithmeticOperation operations = new ArithmeticOperation();
-    //     Integer actual = operations.addOrSub(6, 2);
-    //     Integer expected = 4;
-    //     assertEquals(expected, actual);
+        ArithmeticOperation operations = new ArithmeticOperation();
+        Integer actual = operations.addOrSub(6, 2);
+        Integer expected = 4;
+        assertEquals(expected, actual);
 
-    // }
+    }
 
 }
